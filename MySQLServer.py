aa160ecda769b332690867d1a3cef4a0c4b99540
loisansaa <117663405+loisansaa@users.noreply.github.com>
2025-07-09 15:24:06 +0000

    first commit

--- MySQLServer.py
+++ MySQLServer.py
@@ -17,7 +17,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except mysql.connector.Error :
+    except mysql.connector.Error:
         print(f"Error while connecting to MySQL: {mysql.connector.Error.msg}")
 
     finally:
