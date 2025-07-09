a4a6f23afb84fcdf79841987396caf69996c4b2b
loisansaa <117663405+loisansaa@users.noreply.github.com>
2025-07-09 13:41:58 +0000

    first commit

--- task_2.sql
+++ task_2.sql
@@ -1,38 +1,38 @@
 USE alx_book_store;
 
-CREATE TABLE IF NOT EXISTS authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
+CREATE TABLE IF NOT EXISTS Authors (
+    Author_id INT AUTO_INCREMENT PRIMARY KEY,
+    Author_name VARCHAR(215) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
-    author_id INT,
-    price DOUBLE,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+CREATE TABLE IF NOT EXISTS Books (
+    Book_id INT AUTO_INCREMENT PRIMARY KEY,
+    Title VARCHAR(130) NOT NULL,
+    Author_id INT,
+    Price DOUBLE,
+    Publication_date DATE,
+    FOREIGN KEY (Author_id) REFERENCES Authors(Author_id)
 );
 
-CREATE TABLE IF NOT EXISTS customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215),
-    address TEXT
+CREATE TABLE IF NOT EXISTS Customers (
+    Customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    Customer_name VARCHAR(215) NOT NULL,
+    Email VARCHAR(215),
+    Address TEXT
 );
 
-CREATE TABLE IF NOT EXISTS orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+CREATE TABLE IF NOT EXISTS Orders (
+    Order_id INT AUTO_INCREMENT PRIMARY KEY,
+    Customer_id INT,
+    Order_date DATE,
+    FOREIGN KEY (Customer_id) REFERENCES Customers(Customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS order_details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+CREATE TABLE IF NOT EXISTS Order_details (
+    Orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    Order_id INT,
+    Book_id INT,
+    Quantity DOUBLE,
+    FOREIGN KEY (Order_id) REFERENCES Orders(Order_id),
+    FOREIGN KEY (Book_id) REFERENCES Books(Book_id)
 );
