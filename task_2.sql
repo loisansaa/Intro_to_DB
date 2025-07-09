b118f61ccb775d07ab6ae63571a54cd719353ebe
loisansaa <117663405+loisansaa@users.noreply.github.com>
2025-07-09 13:56:27 +0000

    first commit

--- task_2.sql
+++ task_2.sql
@@ -1,8 +1,8 @@
 USE alx_book_store;
 
 CREATE TABLE IF NOT EXISTS Authors (
-    Author_id INT AUTO_INCREMENT PRIMARY KEY,
-    Author_name VARCHAR(215) NOT NULL
+     author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Books (
