6681a8937d9320219f804839f5fe489320e8c514
loisansaa <117663405+loisansaa@users.noreply.github.com>
2025-07-11 09:07:49 +0000

    commit

--- task_4.sql
+++ task_4.sql
@@ -1,18 +1,12 @@
--- Script to print full description of books table from alx_book_store database
--- Usage: mysql -u username -p alx_book_store < task_4.sql
--- Cannot use DESCRIBE or EXPLAIN statements
-
 SELECT 
     COLUMN_NAME,
-    DATA_TYPE,
+    COLUMN_TYPE,
     IS_NULLABLE,
-    COLUMN_DEFAULT,
     COLUMN_KEY,
+    COLUMN_DEFAULT,
     EXTRA
 FROM 
-    INFORMATION_SCHEMA.COLUMNS 
+    INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books'
-ORDER BY 
-    ORDINAL_POSITION;
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'Books';
